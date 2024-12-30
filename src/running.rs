a283941fbf685242f850cc92df7df0eddbb72a8a
AbrarNitk <abrar.nitk@gmail.com>
2024-12-30 16:03:48 +0530

    main: refactor the code

--- src/running.rs
+++ src/running.rs
@@ -1,1 +1,55 @@
+use bollard::container::{RemoveContainerOptions, StartContainerOptions};
+use bollard::Docker;
 
+pub struct RunningContainer {
+    pub name: String,
+    docker: Docker,
+}
+
+impl RunningContainer {
+    pub async fn start(&self) -> anyhow::Result<()> {
+        start(&self.docker, self.name.as_str()).await?;
+        Ok(())
+    }
+
+    pub async fn remove(&self) -> anyhow::Result<()> {
+        remove(&self.docker, self.name.as_str()).await?;
+        Ok(())
+    }
+
+    pub async fn stop(&self) -> anyhow::Result<()> {
+        stop(&self.docker, self.name.as_str()).await?;
+        Ok(())
+    }
+
+    pub async fn restart(&self) -> anyhow::Result<()> {
+        self.stop().await?;
+        self.start().await?;
+        Ok(())
+    }
+}
+
+pub async fn remove(docker: &Docker, name: &str) -> anyhow::Result<()> {
+    docker
+        .remove_container(
+            name,
+            Some(RemoveContainerOptions {
+                force: true,
+                ..Default::default()
+            }),
+        )
+        .await?;
+    Ok(())
+}
+
+pub async fn stop(docker: &Docker, name: &str) -> anyhow::Result<()> {
+    docker.stop_container(name, None).await?;
+    Ok(())
+}
+
+pub async fn start(docker: &Docker, name: &str) -> anyhow::Result<()> {
+    docker
+        .start_container(name, None::<StartContainerOptions<String>>)
+        .await?;
+    Ok(())
+}
