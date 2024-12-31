e4631014bdfd73b0ce8d91d0fb2af6a8e31db9bc
AbrarNitk <abrar.nitk@gmail.com>
2024-12-31 12:18:07 +0530

    dc mysql

--- src/db/mysql.rs
+++ src/db/mysql.rs
@@ -43,6 +43,44 @@ pub async fn running_container(
     Ok(running_container)
 }
 
+pub async fn running_container_with(
+    container_name: &str,
+    user: &str,
+    pass: &str,
+    db: &str,
+    port: u16,
+    container_reg: Option<String>,
+) -> anyhow::Result<RunningContainer> {
+    let container_name = format!("mysql_{}-{}", container_name, port);
+    let docker_image = docker_image(container_reg);
+
+    let running_container = ContainerRunnerBuilder::new(container_name)
+        .image(docker_image)
+        .add_port_binding(port, 3306)
+        .add_env_var("MYSQL_ROOT_PASSWORD", pass)
+        .add_env_var("MYSQL_DATABASE", db)
+        .add_env_var("MYSQL_USER", user)
+        .add_env_var("MYSQL_PASSWORD", pass)
+        .healthcheck(HealthConfig {
+            test: Some(vec![
+                "CMD-SHELL".to_string(),
+                "mysqladmin ping --host=127.0.0.1 --port=3306 --password=pass".to_string(),
+            ]),
+            interval: Some(250_000_000), // 250ms
+            timeout: Some(100_000_000),  // 100ms
+            retries: Some(5),
+            start_period: Some(500_000_000),
+            start_interval: None,
+        })
+        .build()?
+        .run()
+        .await?;
+
+    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+
+    Ok(running_container)
+}
+
 fn docker_image(container_reg: Option<String>) -> String {
     let cg = container_reg.unwrap_or_else(|| container_registry());
     std::env::var("MYSQL_DOCKER_IMAGE").unwrap_or_else(|_| format!("{}mysql:latest", cg))
@@ -63,6 +101,23 @@ mod tests {
         println!("container 1 is running");
 
         // crate::db::pg::tests::test_pool("postgres://postgres:pass@127.0.0.1:5431/postgres").await?;
+
+        rc.stop().await?;
+        rc.remove().await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_mysql_with() -> anyhow::Result<()> {
+        let rc =
+            super::running_container_with("demo-container-2", "user", "pass", "db", 3307, None)
+                .await
+                .expect("cannot create the container");
+        println!("container 1 is running");
+
+        // mysql -u user --host=127.0.0.1 --database=db --port=3307 --password=pass
+
+        // crate::db::pg::tests::test_pool("mysql://user:pass@127.0.0.1:3306/db").await?;
 
         // rc.stop().await?;
         // rc.remove().await?;
